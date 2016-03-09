d1e5b6584949a495d242101ac5892356bbf1848e
Stephen <saishton@gmail.com>
2016-03-09 22:23:30 +0000

    Change in Method
    Solves system of two parameters instead.

--- wbSolve.m
+++ wbSolve.m
@@ -1,12 +1,12 @@
 function [a,b] = wbSolve(M1,M2)
 
-fnK = (M1^2)/M2;
-
-syms k
-eqn = (gamma(1+(1/k))^2)/(gamma(1+(2/k))) == fnK;
-numK = vpasolve(eqn,k);
-numLam = M1/(gamma(1+(1/numK)));
-
-a=double(numLam);
-b=double(numK);
+syms lambda k
+eqn1 = lambda*gamma(1+(1/k)) == M1;
+eqn2 = lambda^2*gamma(1+(2/k)) == M2;
+eqn = [eqn1, eqn2];
+para = [lambda, k];
+[lambdaSol,kSol] = vpasolve(eqn,para);
+ 
+a=double(lambdaSol);
+b=double(kSol);
 end
