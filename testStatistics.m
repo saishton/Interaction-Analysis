7bb69cc9c3bd5f4eee4181a7e5e9bf0bbece157b
Stephen <saishton@gmail.com>
2016-03-29 11:35:48 +0100

    Optimization
    Switched to a high ratio of 'pre-built' functions to obtain result
    faster.

--- testStatistics.m
+++ testStatistics.m
@@ -1,17 +1,21 @@
 function fit = testStatistics(X,Z)
 
 n = length(X);
-fullecdf = zeros(n+1,1);
-parfor i=1:n
-   thisX = X(i);
-   fullecdf(i+1) =  sum(X <= thisX);
+uni = unique(X);
+if length(uni)==n
+    fullecdf = 0:1/n:1;
+else
+    h = hist(X,X);
+    cumh = cumsum(h);
+    fullecdf = [0 cumh/n];
 end
+fullecdf = fullecdf';
 lowerecdf = fullecdf(1:n);
 upperecdf = fullecdf(2:n+1);
 middlecdf = (lowerecdf+upperecdf)/2;
 
-Dplu = max((lowerecdf/n)-Z);
-Dmin = max(Z-(upperecdf/n));
+Dplu = max(lowerecdf-Z);
+Dmin = max(Z-upperecdf);
 D = max(Dplu,Dmin);
 
 CvM_vec = (Z-middlecdf).^2;
@@ -19,12 +23,12 @@ Wsq = sum(CvM_vec)+(1/(12*n));
 
 V = Dplu+Dmin;
 
-WatMod = n*(sum(Z)/n - 0.5)^2;
+WatMod = n*(mean(Z) - 0.5)^2;
 Usq = Wsq-WatMod;
 
 Zswitch = flipud(Z);
 AD_vec = (2*middlecdf).*(log(Z)+log(1-Zswitch));
-Asq = -sum(AD_vec)/n - n;
+Asq = -sum(AD_vec) - n;
 
 fit = struct(   'Kolmogorov_D_Plus',Dplu,...
                 'Kolmogorov_D_Minus',Dmin,...
